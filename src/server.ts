da148ed039a11dc1288413f555dd16cfa6d3e1da
gabriel-vitebo <vitebo@outlook.com>
2023-12-01 15:04:04 -0300

    :wrench:refactor(env): adding environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import { app } from "./app"
+import { env } from "./env"
 
 app.listen({
   host: '0.0.0.0',
-  port: 3333
+  port: env.PORT,
 }).then(() => {
-  console.log('🟢 HTTP Server is running on Port: 3333 🐶🐱')
+  console.log(`🟢 HTTP Server is running on Port: ${env.PORT} 🐶🐱`)
 })
