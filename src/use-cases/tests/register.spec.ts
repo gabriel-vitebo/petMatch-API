14983e287489d3b24e01c093d3d0047b3956f5bb
gabriel-vitebo <vitebo@outlook.com>
2023-12-08 18:24:35 -0300

    :test_tube: (register use case) add test to register use case

---
+++ src/use-cases/tests/register.spec.ts
@@ -0,0 +1,27 @@
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from '../register'
+import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-user-repository'
+
+describe('Register Use Case', () => {
+  it('should hash user password upon registration', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const { user } = await registerUseCase.execute({
+      name: 'john doe',
+      email: 'johndoe@test.com',
+      password: '123456',
+      cep: 12345678,
+      city: 'South Park',
+      address: 'Ao lado da casa do Cartman',
+    })
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    )
+
+    expect(isPasswordCorrectlyHashed).toBe(true)
+  })
+})
