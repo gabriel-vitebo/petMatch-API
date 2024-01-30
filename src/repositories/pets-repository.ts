a76be44a66d61b383d1b86228eb34b04bc360cc5
gabriel-vitebo <vitebo@outlook.com>
2024-01-30 18:36:29 -0300

    :sparkles: (use case) create fetch pets by characteristics use case

--- src/repositories/pets-repository.ts
+++ src/repositories/pets-repository.ts
@@ -3,10 +3,10 @@ import { Age, Characteristics, Pet, Prisma } from '@prisma/client'
 export interface PetsRepository {
   findManyByCityOfTheOrg(org_id: string[], page: number): Promise<Array<Pet> | []>
   findManyByCharacteristics(
-    age: Age,
-    energyLevel: Characteristics,
-    levelOfIndependence: Characteristics,
-    size: Characteristics
+    age: Age | null,
+    energyLevel: Characteristics | null,
+    levelOfIndependence: Characteristics | null,
+    size: Characteristics | null
   ): Promise<Array<Pet> | []>
   findById(id: string): Promise<Pet | null>
   create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>
