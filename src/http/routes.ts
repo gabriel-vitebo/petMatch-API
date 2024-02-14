9b0ea896c709b9434454c5e696616f98b6b162a2
gabriel-vitebo <vitebo@outlook.com>
2024-02-14 17:00:07 -0300

    :sparkles: (http) generating the org's JWT token

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,12 @@
+import { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticate'
 import { register } from './controllers/register'
-import { FastifyInstance } from 'fastify'
+import { orgProfile } from './controllers/org-profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/orgs', register)
   app.post('/sessions', authenticate)
+
+  /** Authenticated */
+  app.get('/me', orgProfile)
 }
