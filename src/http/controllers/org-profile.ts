af8e5eae6568c0975fce73b8d6e011382210c4ef
gabriel-vitebo <vitebo@outlook.com>
2024-02-16 13:47:45 -0300

    :sparkles: (controllet) creating the controller to display the org profile

--- src/http/controllers/org-profile.ts
+++ src/http/controllers/org-profile.ts
@@ -1,7 +1,17 @@
+import { makeGetOrgProfileUseCase } from '@/use-cases/factories/make-get-org-profile-use-case'
 import { FastifyRequest, FastifyReply } from 'fastify'
 
 export async function orgProfile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
+  const getOrgProfile = makeGetOrgProfileUseCase()
 
-  return reply.status(200).send()
+  const { org } = await getOrgProfile.execute({
+    orgId: request.user.sub,
+  })
+
+  return reply.status(200).send({
+    org: {
+      ...org,
+      password_hash: undefined,
+    },
+  })
 }
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,11 +2,12 @@ import { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticate'
 import { register } from './controllers/register'
 import { orgProfile } from './controllers/org-profile'
+import { verifyJWT } from './middlewares/verify-jwt'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/orgs', register)
   app.post('/sessions', authenticate)
 
   /** Authenticated */
-  app.get('/me', orgProfile)
+  app.get('/me', { onRequest: [verifyJWT] }, orgProfile)
 }
