2c84db758f190580e71f9117f8ae1e32366e04ac
gabriel-vitebo <vitebo@outlook.com>
2024-04-01 15:38:25 -0300

    :white_check_mark: (controller) adding e2e test to the controller for registerin…

--- src/http/controllers/tests/register.spec.ts
+++ src/http/controllers/tests/register.spec.ts
@@ -1,3 +1,28 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => { })
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/orgs').send({
+      personResponsible: 'John Doe',
+      orgName: 'John Doe Pets',
+      email: 'johnDoe@email.com',
+      password: '123456',
+      cep: '12345678',
+      city: 'petLand',
+      address: 'petStreet',
+      phoneNumber: '123456789',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
