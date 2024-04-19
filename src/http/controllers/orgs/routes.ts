2d50916c65d5b82279fe1202f5a41c447329b9c4
gabriel-vitebo <vitebo@outlook.com>
2024-04-19 15:41:33 -0300

    :passport_control: (controller) create refresh token

--- src/http/controllers/orgs/routes.ts
+++ src/http/controllers/orgs/routes.ts
@@ -3,10 +3,13 @@ import { authenticate } from './authenticate'
 import { register } from './register'
 import { orgProfile } from './org-profile'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { refresh } from './refresh'
 
 export async function orgsRoutes(app: FastifyInstance) {
   app.post('/orgs', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   /** Authenticated */
   app.get('/me', { onRequest: [verifyJWT] }, orgProfile)
---
+++ src/http/controllers/orgs/tests/refresh.spec.ts
@@ -0,0 +1,46 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Refresh Token (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to refresh a token', async () => {
+    await request(app.server).post('/orgs').send({
+      personResponsible: 'John Doe',
+      orgName: 'John Doe Pets',
+      email: 'johnDoe@email.com',
+      password: '123456',
+      cep: '12345678',
+      city: 'petLand',
+      address: 'petStreet',
+      phoneNumber: '123456789',
+    })
+
+    const authResponse = await request(app.server).post('/sessions').send({
+      email: 'johnDoe@email.com',
+      password: '123456',
+    })
+
+    const cookies = authResponse.get('Set-Cookie')
+
+    const response = await request(app.server)
+      .patch('/token/refresh')
+      .set('Cookie', cookies ?? [])
+      .send()
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+    expect(response.get('Set-Cookie')).toEqual([
+      expect.stringContaining('refreshToken='),
+    ])
+  })
+})
